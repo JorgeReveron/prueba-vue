2e81b7afac90e8ceeff56b3b550394549b5d03a7
JorgeReveron <jorgealexanderclases@gmail.com>
2024-02-08 17:52:55 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template lang="">
+  <h1>Lista de tareas</h1>
+  <CreateTask @insert-task="insertTask"/>
+  <p>
+    <input type="text" placeholder="nueva tarea" @keyup.enter="insertTask" v-model="newTask">
+  </p>
+  <p>Nuevo id: {{newId}}</p>
+  <p>Se han encontrado {{tasks.length}} tareas</p>
+  <p>{{ textoNumeroTareas }}</p>
+  <ul>
+    <li v-for="task of tasks" 
+      :key="task.id"
+      @click="removeTask(task.id)">
+      {{task.id}} - {{task.name}}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+import CreateTask from './components/CreateTask.vue';
+
+const tasks = ref([
+  {id: 1, name: 'Corregir exámenes del lado del servidor'},
+  {id: 2, name: 'Corregir exámenes del lado del cliente'},
+  {id: 3, name: 'Corregir exámenes de recuperación'},
+  {id: 4, name: 'Preparar examen chungo de Vue'},
+]);
+
+const newTask = ref('');
+
+const textoNumeroTareas = computed(()=>{
+  if (tasks.value.length==0) return "No hay tareas";
+  else if (tasks.value.length==1) return "Hay una tarea";
+  else return "Hay " + tasks.value.length + " tareas."
+});
+
+function insertTask(txt) {
+  console.log("Insertando nueva tarea...");
+  console.log(txt);
+  tasks.value.push({
+    id: newId.value,
+    name: txt
+  });
+}
+
+const newId = computed(()=> {
+  const max = Math.max(...tasks.value.map(t => t.id))+1;
+  return max <0 ? 1 : max;
+});
+
+function removeTask(id) {
+  console.log("Eliminando: " + id);
+  tasks.value = tasks.value.filter(t => t.id!==id);
+}
+</script>
+<style lang="">
+  
+</style>
